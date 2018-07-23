1273b4486e399ef98bfc534d20da19a442f8739e
sebamawa <sebamawa@hotmail.com>
2018-07-23 01:47:49 -0300

    Primer commit

---
+++ apps/categoria/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Categoria(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.CharField(max_length=50)
+    body = models.TextField(max_length=500)
