b661b2a996fdeac1b4a5fc32f53a93ab34fd008d
sebamawa <sebamawa@hotmail.com>
2018-07-24 01:15:44 -0300

    Conexion a Mysql en settings.py

--- ProyectoPruebaDjango/settings.py
+++ ProyectoPruebaDjango/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ProyectoPruebaDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+		'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'NAME': 'django_db_prueba1',
+		'USER': 'root',
+		'PASSWORD': '',
     }
 }
 
