4843ec7216fa8ad9d0cb97a1f1f6bdf6e43048dc
sebamawa <sebamawa@hotmail.com>
2018-08-10 02:21:25 -0300

    Posts apps (Book Django-by-examples)

---
+++ ProyectoPruebaDjango/apps/posts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft')
+        ('published', 'Published')
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    #We specify the name of the reverse relationship, from User to Post,
+    #with the related_name attribute.This will allow us to access related objects easily.
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish',) #descending order by default when we query the database
+
+        def __str__(self):
+            return self.title
