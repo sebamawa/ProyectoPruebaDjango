05e6395009f17df934228ca742bbdf96d31ea614
sebamawa <sebamawa@hotmail.com>
2018-08-11 01:00:50 -0300

    Posts + admin site

--- ProyectoPruebaDjango/apps/posts/admin.py
+++ ProyectoPruebaDjango/apps/posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from ProyectoPruebaDjango.apps.posts.models import Post
 
 # Register your models here.
+
+#admin.site.register(Post) #registra modelo en el admin
+
+@admin.register(Post) #este decorador tambien registra el modelo en el admin
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status') #permite visualizar campos en la lista de posts en el admin
--- ProyectoPruebaDjango/apps/posts/models.py
+++ ProyectoPruebaDjango/apps/posts/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     STATUS_CHOICES = (
-        ('draft', 'Draft')
-        ('published', 'Published')
+        ('draft', 'Draft'),
+        ('published', 'Published'),
     )
 
     title = models.CharField(max_length=250)
